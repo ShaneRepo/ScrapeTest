1273939fe6b0edaa089d2bc45bd141159f0e322f
ShaneRepo <shane8641@live.com>
2017-02-03 10:19:53 -0500

    Testing scraper with BeautifulSoup4 lib

--- scrapetest.py
+++ scrapetest.py
@@ -2,11 +2,21 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
 
-html = urlopen("http://en.wikipedia.org/wiki/Kevin_Bacon")
-bsObj = BeautifulSoup(html)
-for link in bsObj.find("div", {"id":"bodyContent"}).findAll("a",
-                                                            href=re.compile("^(/wiki/)((?!:).)*$")):
-    if 'href' in link.attrs:
-        print(link.attrs['href'])
-# A list of article URLs Keven Bacon links to.
+pages = set()
+def getLinks(pageUrl):
+    global pages
+    html = urlopen("http://en.wikipedia.org"+pageUrl)
+    bsObj = BeautifulSoup(html)
+    for link in bsObj.findAll("a", href=re.compile("^(/wiki/)")):
+        if 'href' in link.attrs:
+            if link.attrs['href'] not in pages:
+                newPage = link.attrs['href']
+                print(newPage)
+                pages.add(newPage)
+                getLinks(newPage)
+getLinks("")
+
+
+
+
 
