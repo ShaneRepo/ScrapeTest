3e0307e7850e2ffeaa5b539fd940ff38fa0a2d14
ShaneRepo <shane8641@live.com>
2017-02-06 09:24:54 -0500

    Testing scraper with BeautifulSoup4 lib

--- scrapetest.py
+++ scrapetest.py
@@ -1,20 +1,12 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-import re
-
-pages = set()
-def getLinks(pageUrl):
-    global pages
-    html = urlopen("http://en.wikipedia.org"+pageUrl)
-    bsObj = BeautifulSoup(html)
-    for link in bsObj.findAll("a", href=re.compile("^(/wiki/)")):
-        if 'href' in link.attrs:
-            if link.attrs['href'] not in pages:
-                newPage = link.attrs['href']
-                print(newPage)
-                pages.add(newPage)
-                getLinks(newPage)
-getLinks("")
+import pymysql
+conn = pymysql.connect(host='127.0.0.1', unix_socket='/tmp/mysql.sock', user='root', passwd='Jocko8696', db='mysql')
+cur = conn.cursor()
+cur.execute("USE scraping")
+cur.execute("SELECT * FROM pages WHERE id=1")
+print(cur.fetchone())
+cur.close()
+conn.close()
+# Using PyMySQL to interact with database
 
 
 
